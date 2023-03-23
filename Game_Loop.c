ba28cffe53a1c6312a78498114d04762d89aa9a0
SeniourMarquies <sap_al@hotmail.com>
2023-03-23 19:21:54 +0300

    Fixed ball and paddle issue

--- Game_Loop.c
+++ Game_Loop.c
@@ -57,21 +57,18 @@ void process_input(void) {
     game_is_running = false;
     break;
   case SDL_KEYDOWN:
-    if (event.key.keysym.sym == SDLK_ESCAPE) {
+    if (event.key.keysym.sym == SDLK_ESCAPE)
       game_is_running = false;
-    }
-    // TODO: Set paddle velocity based on left/right arrow keys
-    // ...
     if (event.key.keysym.sym == SDLK_LEFT)
-      paddle.vel_x -= 10.0;
+      paddle.vel_x = -400;
     if (event.key.keysym.sym == SDLK_RIGHT)
-      paddle.vel_x += 10.0;
-   
+      paddle.vel_x = +400;
     break;
   case SDL_KEYUP:
-    // TODO: Reset paddle velocity based on left/right arrow keys
-    // ...
-   
+    if (event.key.keysym.sym == SDLK_LEFT)
+      paddle.vel_x = 0;
+    if (event.key.keysym.sym == SDLK_RIGHT)
+      paddle.vel_x = 0;
     break;
   }
 }
@@ -80,14 +77,15 @@ void process_input(void) {
 // Setup function that runs once at the beginning of our program
 ///////////////////////////////////////////////////////////////////////////////
 void setup(void) {
-  // Initialize the ball object moving down at a constant velocity
+  // Initialize values for the the ball object
   ball.width = 15;
   ball.height = 15;
   ball.x = 20;
   ball.y = 20;
   ball.vel_x = 300;
   ball.vel_y = 300;
-  // Initialize the paddle position at the bottom of the screen
+
+  // Initialize the values for the paddle object
   paddle.width = 100;
   paddle.height = 20;
   paddle.x = (WINDOW_WIDTH / 2) - (paddle.width / 2);
@@ -107,60 +105,41 @@ void update(void) {
   if (time_to_wait > 0 && time_to_wait <= FRAME_TARGET_TIME)
     SDL_Delay(time_to_wait);
 
-  // Get delta_time factor converted to seconds to be used to update objects
+  // Get a delta time factor converted to seconds to be used to update my
+  // objects
   float delta_time = (SDL_GetTicks() - last_frame_time) / 1000.0;
 
-  // Store the milliseconds of the current frame to be used in the next one
+  // Store the milliseconds of the current frame
   last_frame_time = SDL_GetTicks();
 
-  // Update ball position based on its velocity
+  // update ball and paddle position
   ball.x += ball.vel_x * delta_time;
   ball.y += ball.vel_y * delta_time;
-
-  // TODO: Update paddle position based on its velocity
-  // ...
   paddle.x += paddle.vel_x * delta_time;
   paddle.y += paddle.vel_y * delta_time;
 
-  // TODO: Check for ball collision with the walls
-  // ...
-  if (ball.x >= WINDOW_WIDTH - 1)
-    ball.vel_x = -300;
-  if (ball.x <= 0)
-    ball.vel_x = 300;
-  if (ball.y >= WINDOW_HEIGHT - 1)
-    ball.vel_y = -300;
-  if (ball.y <= 0)
-    ball.vel_y = 300;
- 
+  // Check for ball collision with the walls
+  if (ball.x <= 0 || ball.x + ball.width >= WINDOW_WIDTH)
+    ball.vel_x = -ball.vel_x;
+  if (ball.y < 0)
+    ball.vel_y = -ball.vel_y;
 
+  // Check for ball collision with the paddle
+  if (ball.y + ball.height >= paddle.y && ball.x + ball.width >= paddle.x &&
+      ball.x <= paddle.x + paddle.width)
+    ball.vel_y = -ball.vel_y;
 
-  // TODO: Check for ball collision with the paddle
-  // ...
-  if ((ball.y == paddle.y - 1) && (ball.x >= paddle.x - 1) &&
-      (ball.x < paddle.x + paddle.width + 1))
-  {
-    if (ball.x < paddle.x + paddle.width / 2)
-      ball.vel_x = -300;
-    else
-      ball.vel_x = 300;
-
-    ball.vel_y = -300;
-  }
-
-
-
-  // TODO: Prevent paddle from moving outside the boundaries of the window
-  // ...
-  if (paddle.x < 0)
+  // Prevent paddle from moving outside the boundaries of the window
+  if (paddle.x <= 0)
     paddle.x = 0;
-  if (paddle.x >= WINDOW_WIDTH - 1)
+  if (paddle.x >= WINDOW_WIDTH - paddle.width)
     paddle.x = WINDOW_WIDTH - paddle.width;
 
-  // TODO: Check for game over when ball hits the bottom part of the screen
-  // ...
-  if (ball.y >= WINDOW_HEIGHT - 1)
-    ball.y = WINDOW_HEIGHT / 2;
+  // Check for game over
+  if (ball.y + ball.height > WINDOW_HEIGHT) {
+    ball.x = WINDOW_WIDTH / 2;
+    ball.y = 0;
+  }
 }
 
 ///////////////////////////////////////////////////////////////////////////////
